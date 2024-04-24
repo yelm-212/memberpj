d61bd4ce5d847dfef1e9ecc173558654126761b1
yelm <21yrshin@naver.com>
2024-04-24 11:22:22 +0900

    Initial commit

---
+++ src/main/java/com/yelm/memberpj/MemberpjApplication.java
@@ -0,0 +1,13 @@
+package com.yelm.memberpj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberpjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemberpjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yelm/memberpj/MemberpjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yelm.memberpj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberpjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
