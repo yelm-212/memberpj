4b810e9cbdb0021e1c2ec19720e2c57ad1ebbaf6
yelm <21yrshin@naver.com>
2024-04-25 13:22:45 +0900

    feat: Added data.sql, schema.sql

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,9 @@
+create table members(
+                        member_id bigint not null,
+                        password varchar(256) not null,
+                        nickname varchar(100) not null,
+                        name varchar(100) not null,
+                        phonenumber varchar(256) not null,
+                        email varchar(256) not null,
+                        primary key(member_id)
+);
