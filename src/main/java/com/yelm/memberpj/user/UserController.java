01b38b618354aa27bf212cd7a8a6bc8a16b63d8b
yelm <21yrshin@naver.com>
2024-04-26 02:35:54 +0900

    feat: UserController Implemented
    - 세부 로직 구현 서비스 레이어에서 (TODO 확인)

--- src/main/java/com/yelm/memberpj/user/UserController.java
+++ src/main/java/com/yelm/memberpj/user/UserController.java
@@ -1,11 +1,43 @@
 package com.yelm.memberpj.user;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.constraints.Positive;
 
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
     //TODO: Controller Implementation
+
+    @GetMapping("/hello")
+    public String test(){
+        return "hello";
+    }
+
+    @PostMapping("/join")
+    public ResponseEntity signup(@RequestBody UserDto.UserJoinDto userJoinDto){
+        // Todo: UserDto, Service Layer Implementation
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity getUserList(@Positive @RequestParam int page,
+                                    @Positive @RequestParam int pageSize,
+                                    @RequestParam(required = false, defaultValue = "") String sort){
+        // Todo: Service Layer Implementation
+
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @GetMapping("/{user-id}")
+    public ResponseEntity updateUser(@PathVariable("user-id") @Positive String userId){
+        // Todo: Service Layer Implementation
+
+        // Todo: updated user info 함께 전달
+        return new ResponseEntity(HttpStatus.ACCEPTED);
+    }
 }
