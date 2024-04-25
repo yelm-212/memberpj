75b5ec7b6086b5daffc7819e473db42aac78b8c2
yelm <21yrshin@naver.com>
2024-04-25 15:15:13 +0900

    feat: URICreator Implementation

--- src/main/java/com/yelm/memberpj/utils/URICreator.java
+++ src/main/java/com/yelm/memberpj/utils/URICreator.java
@@ -1,2 +1,15 @@
-package com.yelm.memberpj.utils;public class URICreator {
+package com.yelm.memberpj.utils;
+
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+public class URICreator {
+    public static URI createUri(String defaultUrl, long resourceId) {
+        return UriComponentsBuilder
+                .newInstance()
+                .path(defaultUrl + "/{resource-id}")
+                .buildAndExpand(resourceId)
+                .toUri();
+    }
 }
